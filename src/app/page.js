0f236e30e17012b530206e324936ab8b34779bde
moabe <momoko.abe.peach@gmail.com>
2025-05-28 16:35:59 +0900

    username step

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 'use client'; 
 import Image from "next/image";
+import { useState } from 'react';
 import styles from "./page.module.css";
 import Header_home from "../../components/Header_home";
 import Square from "../../components/Square";
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const [step, setStep] = useState(1);
+  const [username, setUsername] = useState('');
+  const [partnername, setPartnername] = useState('');
+  const router = useRouter();
+
   return (
     <div className={`${styles.page } ${styles.background}`}>
       <Header_home />
@@ -13,17 +19,25 @@ export default function Home() {
       <main className={`${styles.main}`}>
         <div className={styles.formBlock}>
            <p className={styles.heading}>Let's talk!</p>
-          <div>
-            自分の名前<br />
-            <input type="text" name="username" className={styles.customInput} />
-          </div>
-          <div>
-            相手の名前<br />
-            <input type="text" name="partnername" className={styles.customInput} />
-          </div>
+          {step === 1 && (
+            <>
+              <div>Your Name</div>
+              <input value={username} onChange={(e) => setUsername(e.target.value)} className={styles.customInput}/>
+              <button onClick={() => setStep(2)}  className={styles.talk}>Continue</button>
+            </>
+          )}
+
+          {step === 2 && (
+            <>
+              <div>相手の名前</div>
+              <input value={partnername} onChange={(e) => setPartnername(e.target.value)} className={styles.customInput}/>
+              <button onClick={() => router.push('/talk')}  className={styles.talk}>Talk</button>
+            </>
+          )}
+
           <talk/>
         </div>
-        <Page className={styles.talk}/>
+        {/* <Page className={styles.talk}/> */}
         <div className={styles.ctas}></div>
       </main>
       <footer className={styles.footer}></footer>
@@ -31,20 +45,20 @@ export default function Home() {
   );
 }
 
-export function Page({ className }) {
-  const router = useRouter();
+// export function Page({ className }) {
+//   const router = useRouter();
 
-  const handleClick = () => {
-    router.push('/talk');
-  };
+//   const handleClick = () => {
+//     router.push('/talk');
+//   };
 
-  return (
-    <div>
-      <button onClick={handleClick} className={className}>
-        Talk
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <button onClick={handleClick} className={className}>
+//         Talk
+//       </button>
+//     </div>
+//   );
+// }
 
 
