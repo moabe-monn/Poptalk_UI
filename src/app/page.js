42cb84aa19c273cd8b3e689b50aef8a1beec5e1c
moabe <momoko.abe.peach@gmail.com>
2025-05-28 17:01:34 +0900

    change interval

--- src/app/page.js
+++ src/app/page.js
@@ -17,8 +17,7 @@ export default function Home() {
       <Header_home />
         <Square />
       <main className={`${styles.main}`}>
-        <div className={styles.formBlock}>
-           <p className={styles.heading}>Let's talk!</p>
+          <p className={styles.heading}>Let's talk!</p>
           {step === 1 && (
             <>
               <div>Your Name</div>
@@ -29,36 +28,15 @@ export default function Home() {
 
           {step === 2 && (
             <>
-              <div>相手の名前</div>
+              <div>Parter's Name</div>
               <input value={partnername} onChange={(e) => setPartnername(e.target.value)} className={styles.customInput}/>
               <button onClick={() => router.push('/talk')}  className={styles.talk}>Talk</button>
             </>
           )}
-
-          <talk/>
-        </div>
-        {/* <Page className={styles.talk}/> */}
-        <div className={styles.ctas}></div>
       </main>
       <footer className={styles.footer}></footer>
     </div>
   );
 }
-
-// export function Page({ className }) {
-//   const router = useRouter();
-
-//   const handleClick = () => {
-//     router.push('/talk');
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={handleClick} className={className}>
-//         Talk
-//       </button>
-//     </div>
-//   );
-// }
 
 
