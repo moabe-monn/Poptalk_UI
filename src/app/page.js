e724e08694f31adeaaf77662a57b639c36aae9f4
moabe <momoko.abe.peach@gmail.com>
2025-05-31 00:02:20 +0900

    make talkpage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
 'use client'; 
-import Image from "next/image";
 import { useState } from 'react';
 import styles from "./page.module.css";
 import Header_home from "../../components/Header_home";
-import Square from "../../components/Square";
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
@@ -21,25 +19,24 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&display=swap" rel="stylesheet" />
         <Header_home />
-        {/* <Square /> */}
-        <main className={`${styles.main}`}>
-            <p className={styles.heading}>Let's talk!</p>
-            {step === 1 && (
-              <>
-                <div>Your Name</div>
-                <input value={username} onChange={(e) => setUsername(e.target.value)} className={styles.customInput}/>
-                <button onClick={() => setStep(2)}  className={styles.talk}>Continue</button>
-              </>
-            )}
+          <main className={`${styles.main}`}>
+              <p className={styles.heading}>Let's talk!</p>
+              {step === 1 && (
+                <>
+                  <div>Your Name</div>
+                  <input value={username} onChange={(e) => setUsername(e.target.value)} className={styles.customInput}/>
+                  <button onClick={() => setStep(2)}  className={styles.talk}>Continue</button>
+                </>
+              )}
 
-            {step === 2 && (
-              <>
-                <div>Parter's Name</div>
-                <input value={partnername} onChange={(e) => setPartnername(e.target.value)} className={styles.customInput}/>
-                <button onClick={() => router.push('/talk')}  className={styles.talk}>Talk</button>
-              </>
-            )}
-        </main>
+              {step === 2 && (
+                <>
+                  <div>Parter's Name</div>
+                  <input value={partnername} onChange={(e) => setPartnername(e.target.value)} className={styles.customInput}/>
+                  <button onClick={() => router.push(`/talk?partnername=${encodeURIComponent(partnername)}`)}  className={styles.talk}>Talk</button>
+                </>
+              )}
+          </main>
       </div>
     </div>
   );
--- src/app/talk/page.js
+++ src/app/talk/page.js
@@ -1,14 +1,25 @@
+'use client';
+import { useSearchParams } from 'next/navigation';
 import Talklog from "../../../components/talklog";
 import styles from "../page.module.css";
+import talk_style from "./page_talk.module.css";
 
-export default function Home() {
+export default function Talk() {
+	const searchParams = useSearchParams();
+  	const partnername = searchParams.get('partnername');
+
 	return (
 		<div className={`${styles.page } ${styles.background}`}>
 			<div className={`${styles.square3}`}></div>
       		<div className={`${styles.square2}`}></div>
 			<div className={`${styles.square}`}>
-				<div>
-					<Talklog />
+				<div className={`${talk_style.headersquare}`}>
+					<div className={`${talk_style.header}`}>
+						{` ${partnername}`}
+					</div>
+				</div>
+				<div className={`${talk_style.footer}`}>
+
 				</div>
 			</div>
 		</div>
